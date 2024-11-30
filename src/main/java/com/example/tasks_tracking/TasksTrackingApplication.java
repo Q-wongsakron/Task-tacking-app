1c73973cfbc95bf3462f492e0bbaae3ddcb26ddb
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2024-11-30 14:16:59 +0700

    first commit

---
+++ src/main/java/com/example/tasks_tracking/TasksTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasks_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksTrackingApplication.class, args);
+	}
+
+}
